46c7acbacd527fbb1b438c054dd6d53d0e20248a
Aldrin Capuchino <101033787+pnkskff@users.noreply.github.com>
2022-11-14 15:52:10 -0500

    Update cocktail.js

--- Cocktail App/assets/js/cocktail.js
+++ Cocktail App/assets/js/cocktail.js
@@ -1,15 +1,16 @@
 document.querySelector('#cheers').addEventListener('click', getDrink)
 
 function getDrink(){
-    let drink = document.querySelector('input').value
 
-   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-      .then(res => res.json()) // parse response as JSON
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+
+      .then(res => res.json())
       .then(data => {
       console.log(data.drinks)
-      document.querySelector('#drinkName').innerText = data.drinks[3].strDrink
-      document.querySelector('img').src = data.drinks[3].strDrinkThumb
-      document.querySelector('#instructions').innerText = data.drinks[3].strInstructions
+      document.querySelector('#drinkName').innerText = data.drinks[0].strDrink
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      document.querySelector('#ingredients').innerText = data.drinks[0].strIngredient1
+      document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
     })
     .catch(err => {
         console.log(`error ${err}`)
